39fb53c6f3f2ed406daa7d1aad944ada82d4ae2d
funbiscuit <kokurinsv@gmail.com>
2021-09-07 19:54:30 +0300

    Refactor binnEncode
    Split encoding of different types to different functions
    Remove multiple if-else branches for checking what function to use

--- binnEncode.c
+++ binnEncode.c
@@ -5,7 +5,21 @@
 
 #include "binn.h"
 
-#define TYPE_MAP_LEN (sizeof(typeMap)/sizeof(*typeMap))
+#define ARRAY_SIZE(arr) (sizeof(arr)/sizeof((arr)[0]))
+
+typedef struct {
+    /**
+     * Condition whether encoder supports encoding given object
+     * @param obj
+     */
+    bool ( *predicate)(const mxArray *obj);
+
+    /**
+     * Function that should encode given object
+     * @param obj
+     */
+    binn *( *encoder)(const mxArray *obj);
+} Encoder;
 
 // map for easier conversion from matlab numeric to binn values
 static int typeMap[][2] = {
@@ -21,108 +35,23 @@ static int typeMap[][2] = {
         {BINN_DOUBLE, mxDOUBLE_CLASS},
 };
 
-binn *encodeScalar(const mxArray *obj) {
-    mxClassID classId = mxGetClassID(obj);
-    int type = 0;
+static binn *encode(const mxArray *obj);
 
-    for (int i = 0; i < TYPE_MAP_LEN; ++i) {
-        if (typeMap[i][1] == classId) {
-            type = typeMap[i][0];
-            break;
-        }
-    }
+static binn *encodeStruct(const mxArray *obj);
 
-    if (type != 0) {
-        return binn_value(type, mxGetPr(obj), 0, NULL);
-    }
+static binn *encodeNumeric(const mxArray *obj);
 
-    return NULL;
-}
+static binn *encodeString(const mxArray *obj);
 
-binn *encodeArray(const mxArray *obj) {
-    mxClassID classId = mxGetClassID(obj);
-    int type = 0;
+static binn *encodeScalar(const mxArray *obj);
 
-    for (int i = 0; i < TYPE_MAP_LEN; ++i) {
-        if (typeMap[i][1] == classId) {
-            type = typeMap[i][0];
-            break;
-        }
-    }
+static binn *encodeArray(const mxArray *obj);
 
-    if (type == 0) {
-        return NULL;
-    }
-
-    binn *list = binn_list();
-
-    uint8_t *data = (uint8_t *) mxGetPr(obj);
-
-    int elemSize = mxGetElementSize(obj);
-
-    mwSize cols = mxGetN(obj);
-    for (int i = 0; i < cols; ++i) {
-        binn_list_add(list, type, &data[i * elemSize], 0);
-    }
-
-    return list;
-}
-
-binn *encode(const mxArray *obj) {
-    //TODO error checking
-    binn *encoded = NULL;
-
-    if (mxIsStruct(obj)) {
-        encoded = binn_object();
-
-        int fieldNum = mxGetNumberOfFields(obj);
-        //TODO check if struct is empty?
-        for (int i = 0; i < fieldNum; ++i) {
-            mxArray *value = mxGetFieldByNumber(obj, 0, i);
-            const char *name = mxGetFieldNameByNumber(obj, i);
-
-            binn *bval = encode(value);
-
-            if (bval != NULL) {
-                binn_object_set_value(encoded, name, bval);
-                binn_free(bval);
-            } else {
-                mexPrintf("Skipped field '%s' with unknown or invalid value\n", name);
-            }
-        }
-    } else if (mxIsNumeric(obj)) {
-        if (mxIsComplex(obj)) {
-            mexWarnMsgTxt("Complex data is not supported");
-            return NULL;
-        } else if (mxIsSparse(obj)) {
-            mexWarnMsgTxt("Sparse data is not supported");
-            return NULL;
-        }
-        mwSize ndims = mxGetNumberOfDimensions(obj);
-        if (ndims > 2) {
-            mexWarnMsgTxt("Multidimensional data is not supported");
-            return NULL;
-        }
-        mwSize rows = mxGetM(obj);
-        mwSize cols = mxGetN(obj);
-        if (rows > 1) {
-            mexWarnMsgTxt("Only row-vectors are supported");
-            return NULL;
-        }
-
-        if (cols == 1) {
-            encoded = encodeScalar(obj);
-        } else {
-            encoded = encodeArray(obj);
-        }
-    } else if (mxIsChar(obj)) {
-        char *str = mxArrayToString(obj);
-        encoded = binn_string(str, BINN_TRANSIENT);
-        mxFree(str);
-    }
-
-    return encoded;
-}
+static Encoder encoders[] = {
+        {mxIsStruct,  encodeStruct},
+        {mxIsNumeric, encodeNumeric},
+        {mxIsChar,    encodeString},
+};
 
 void mexFunction(int nlhs, mxArray *plhs[],
                  int nrhs, const mxArray *prhs[]) {
@@ -153,4 +82,118 @@ void mexFunction(int nlhs, mxArray *plhs[],
     memcpy(mxGetPr(plhs[0]), binn_ptr(obj), dims[1]);
 
     binn_free(obj);
+}
+
+static binn *encode(const mxArray *obj) {
+    //TODO error checking
+    if (mxIsComplex(obj)) {
+        mexWarnMsgTxt("Complex data is not supported");
+        return NULL;
+    } else if (mxIsSparse(obj)) {
+        mexWarnMsgTxt("Sparse data is not supported");
+        return NULL;
+    }
+
+    for (int i = 0; i < ARRAY_SIZE(encoders); ++i) {
+        if (encoders[i].predicate(obj)) {
+            return encoders[i].encoder(obj);
+        }
+    }
+
+    return NULL;
+}
+
+static binn *encodeStruct(const mxArray *obj) {
+    binn *encoded = binn_object();
+
+    int fieldNum = mxGetNumberOfFields(obj);
+    //TODO check if struct is empty?
+    for (int i = 0; i < fieldNum; ++i) {
+        mxArray *value = mxGetFieldByNumber(obj, 0, i);
+        const char *name = mxGetFieldNameByNumber(obj, i);
+
+        binn *bval = encode(value);
+
+        if (bval != NULL) {
+            binn_object_set_value(encoded, name, bval);
+            binn_free(bval);
+        } else {
+            mexPrintf("Skipped field '%s' with unknown or invalid value\n", name);
+        }
+    }
+    return encoded;
+}
+
+static binn *encodeNumeric(const mxArray *obj) {
+    mwSize ndims = mxGetNumberOfDimensions(obj);
+    if (ndims > 2) {
+        mexWarnMsgTxt("Multidimensional data is not supported");
+        return NULL;
+    }
+    size_t rows = mxGetM(obj);
+    size_t cols = mxGetN(obj);
+    if (rows > 1) {
+        mexWarnMsgTxt("Only row-vectors are supported");
+        return NULL;
+    }
+
+    if (cols == 1) {
+        return encodeScalar(obj);
+    } else {
+        return encodeArray(obj);
+    }
+}
+
+static binn *encodeString(const mxArray *obj) {
+    char *str = mxArrayToString(obj);
+    binn *encoded = binn_string(str, BINN_TRANSIENT);
+    mxFree(str);
+    return encoded;
+}
+
+static binn *encodeScalar(const mxArray *obj) {
+    mxClassID classId = mxGetClassID(obj);
+    int type = 0;
+
+    for (int i = 0; i < ARRAY_SIZE(typeMap); ++i) {
+        if (typeMap[i][1] == classId) {
+            type = typeMap[i][0];
+            break;
+        }
+    }
+
+    if (type != 0) {
+        return binn_value(type, mxGetPr(obj), 0, NULL);
+    }
+
+    return NULL;
+}
+
+static binn *encodeArray(const mxArray *obj) {
+    mxClassID classId = mxGetClassID(obj);
+    int type = 0;
+
+    for (int i = 0; i < ARRAY_SIZE(typeMap); ++i) {
+        if (typeMap[i][1] == classId) {
+            type = typeMap[i][0];
+            break;
+        }
+    }
+
+    if (type == 0) {
+        return NULL;
+    }
+
+    binn *list = binn_list();
+
+    uint8_t *data = (uint8_t *) mxGetPr(obj);
+
+    size_t elemSize = mxGetElementSize(obj);
+
+    size_t cols = mxGetN(obj);
+    for (int i = 0; i < cols; ++i) {
+        binn_list_add(list, type, &data[i * elemSize], 0);
+    }
+
+    return list;
 }
